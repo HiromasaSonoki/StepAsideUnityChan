f062d19d950713e7798dd19290e83b3483cb8ece
HiromasaSonoki <cehi21106@g.nihon-u.ac.jp>
2023-06-19 09:52:23 +0900

    Initial commit

---
+++ Assets/DestroyCubeController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCubeController : MonoBehaviour
+{
+    //cubeを移動させるコンポーネントを入れる
+    private Rigidbody myRigidbody;
+    //前方向の速度
+    private float velocityZ = 16f;
+    //動きを減速させる係数（追加）
+    private float coefficient = 0.99f;
+
+    private UnityChanController unityChanController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Rigidbodyコンポーネントを取得（追加）
+        this.myRigidbody = GetComponent<Rigidbody>();
+
+        unityChanController = GameObject.Find("unitychan").GetComponent<UnityChanController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Unityちゃんに速度を与える
+        this.myRigidbody.velocity = new Vector3(0, 0, velocityZ);
+
+        if (unityChanController.isEnd)
+        {
+            this.velocityZ *= this.coefficient;
+           
+            
+        }
+
+        void OnCollisionEnter(Collision other)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
